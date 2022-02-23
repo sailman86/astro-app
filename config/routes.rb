0d0c429f551e6d4b4d48d11ae10b485f148c4b21
Svetoslav Pachev <svetoslavpachevvv@gmail.com>
2022-02-23 11:49:46 +0100

    created routes for booking

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'astronauts#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :astronauts, only: [:show]
+  resources :astronauts, only: [:show] do
+    resources :bookings, only: [:new, :create, :show]
+  end
 end
