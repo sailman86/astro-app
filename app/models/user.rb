1a5f00c528258b00ca2d41084001a6eb91353ead
Svetoslav Pachev <svetoslavpachevvv@gmail.com>
2022-02-23 14:48:39 +0100

    we created new booking view and controller action

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
   has_many :bookings, dependent: :destroy
   has_many :astronauts, dependent: :destroy
 
-  validates :name, presence: true
   validates :email, presence: true, uniqueness: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
