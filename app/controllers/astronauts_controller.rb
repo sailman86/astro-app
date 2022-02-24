b6ba64bbcdbb843bb5e38819462617078705f50f
Kristijan Federer <federer.kristijan@gmail.com>
2022-02-24 11:57:37 +0100

    added astronauts methods new, create, destroy. added routes

--- app/controllers/astronauts_controller.rb
+++ app/controllers/astronauts_controller.rb
@@ -1,9 +1,35 @@
 class AstronautsController < ApplicationController
+  before_action :astronauts, only: [:show, :destroy]
   def index
     @astronauts = Astronaut.all
   end
 
   def show
-    @astronaut = Astronaut.find(params[:id])
+  end
+
+  def new
+    @astronaut = Astronaut.new
+  end
+
+  def create
+    @astronaut = Astronaut.create(astronaut_params[:astronaut])
+
+    redirect_to astronaut_path(@astronaut)
+  end
+
+  def destroy
+    @astronaut.destroy
+
+    redirect_to astronaut_path
+  end
+
+  private
+
+  def set_astronaut
+    @astronaut = Astronaut.find(astronaut_params[:id])
+  end
+  
+  def astronaut_params
+    params.require(:astronaut).permit(:name, :bio, :nationality)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'astronauts#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :astronauts, only: [:show] do
-    resources :bookings, only: [:new, :create]
+  resources :astronauts, only: [:show, :new, :create, :destroy] do
+    resources :bookings, only: [:new, :create, ]
   end
   resources :bookings, only: [:show]
 end
