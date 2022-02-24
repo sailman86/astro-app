d283098ce60862a4a80c0266de745cdb2a3397d4
Arne Schmidt <arne.schmidt.eisenach@gmx.de>
2022-02-24 17:53:37 +0100

    created delete

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'astronauts#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :astronauts, only: [:show, :new, :create, :destroy] do
+  resources :astronauts, only: [:show, :new, :create] do
     resources :bookings, only: [:new, :create]
   end
   resources :bookings, only: [:show]
+  resources :astronauts, only: [:destroy]
 end
